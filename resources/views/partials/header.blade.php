ed9dfe3825397b6cd0212aff0576cfe037a86727
Michela-DC <micheladelconte@outlook.com>
2022-04-19 20:55:57 +0200

    Added header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,49 @@
+<header>
+    <div class="header-container">
+      <figure class="logo-wrapper">
+            <img class="logo" src="../../img/dc-logo.png" alt="">
+      </figure>
+
+      <ul class="navbar">
+            <li>
+                <a href="">characters</a>
+            </li>
+
+            <li>
+                <a href="">comics</a>
+            </li>
+
+            <li>
+                <a href="">movies</a>
+            </li>
+
+            <li>
+                <a href="">tv</a>
+            </li>
+            
+            <li>
+                <a href="">games</a>
+            </li>
+
+            <li>
+                <a href="">collectibles</a>
+            </li>
+
+            <li>
+                <a href="">videos</a>
+            </li>
+
+            <li>
+                <a href="">fans</a>
+            </li>
+
+            <li>
+                <a href="">news</a>
+            </li>
+
+            <li>
+                <a href="">shop</a>
+            </li>
+      </ul>
+    </div>
+</header>
